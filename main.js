aa1be59c7acb8b3aea8fff9d5f8d8ae03ba91e70
Geoseanlee <lee.zixi.george@outlook.com>
2025-07-10 20:35:31 +1000

    reset main

--- main.js
+++
@@ -1,9 +0,0 @@
-function createMessage(phoneNumber, message) {
-    return {
-        phoneNumber: phoneNumber,
-        message: message,
-        messageLength: message.length,
-    };
-}
-
-export { createMessage };
