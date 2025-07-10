c53c76f243a49644157c82752dfa952a5a019846
Geoseanlee <lee.zixi.george@outlook.com>
2025-07-10 20:38:53 +1000

    C4L1

---
+++ main.js
@@ -0,0 +1,9 @@
+function createMessage(phoneNumber, message) {
+    return {
+        phoneNumber: phoneNumber,
+        message: message,
+        messageLength: message.length,
+    };
+}
+
+export { createMessage };
