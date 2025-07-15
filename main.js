9e4e413f9c0f019c99c492cf1a122e86ff06203c
Geoseanlee <lee.zixi.george@outlook.com>
2025-07-15 21:08:28 +1000

    Solved

---
+++ main.js
@@ -0,0 +1,12 @@
+function getProviderCount(provider, counts) {
+
+    if (!counts[provider]) {
+        return 0;
+    }
+    return counts[provider];
+
+}
+
+// don't touch below this line
+
+export { getProviderCount };
